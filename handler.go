bc44f3eacb4ea6ecd28948149cc3cb7829b24537
pietroid-lab <pietrodipaulo4@gmail.com>
2025-10-27 05:49:51 -0300

    first commit, creating handler and init go mod

---
+++ handler.go
@@ -0,0 +1,12 @@
+package main
+
+import (
+	"net/http"
+
+	"github.com/gin-gonic/gin"
+)
+
+func getAlbums(c *gin.Context) {
+	c.IndentedJSON(http.StatusOK, url)
+
+}
---
+++ models.go
@@ -0,0 +1,7 @@
+package main
+
+type Response struct {
+	Url     string  `json:"URL"`
+}
+
+var url = "https://www.linkedin.com/in/pietro-caproni-h/"
