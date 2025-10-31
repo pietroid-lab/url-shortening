4efb074efbca7ebeaca9e691d3f6c79ea63534ed
pietroid-lab <pietrodipaulo4@gmail.com>
2025-10-31 11:39:33 -0300

    create connection with mongodb

--- cmd/main.go
+++ cmd/main.go
@@ -1,16 +1,33 @@
 package main
 
 import (
+	"context"
+	"log"
+	"os"
 	"url-shorting/cmd/api/handler"
 	"url-shorting/cmd/internal"
 
 	"github.com/gin-gonic/gin"
+	"go.mongodb.org/mongo-driver/mongo"
+	"go.mongodb.org/mongo-driver/mongo/options"
 )
 
 func main() {
 	// Set up Gin router and dependencies
+
+	ctx := context.Background()
+
 	router := gin.Default()
-	repo := &internal.Repository{}
+
+	serverAPI := options.ServerAPI(options.ServerAPIVersion1)
+	uri := os.Getenv("MONGO_URI")
+	mongoCLinet, err := mongo.Connect(ctx, options.Client().ApplyURI(uri).SetServerAPIOptions(serverAPI))
+	if err != nil {
+		log.Fatal("Error connecting to MongoDB:", err)
+		panic(err)
+	}
+	
+	repo := internal.NewRepository(mongoCLinet)
 	svc := internal.NewService(repo)
 	h := handler.NewHandler(svc)
 
